c842fefb27e1d0c4fabc6880f2e2e0b735c96ae6
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2024-04-24 03:39:36 +0530

    add header, add panel strucuture

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="bg-slate-200 py-2 px-4 text-end ">
+      <button className="bg-white border border-blue-700 text-blue-700 font-bold py-2 px-4 rounded md:mr-16">
+        Save Changes
+      </button>
+    </div>
+  );
+};
